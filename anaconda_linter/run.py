5ea0d75cd1d54eab19f057914b7b3bcfa906324f
Charles Bousseau <cbousseau@anaconda.com>
2022-08-11 09:36:59 -0400

    update linter init

--- anaconda_linter/run.py
+++ anaconda_linter/run.py
@@ -6,12 +6,13 @@ import pdb
 if __name__ == "__main__":
     config = utils.load_config("./config.yaml")
 
-    recipe_folder = f"{sys.argv[1]}/{sys.argv[2]}-feedstock/recipe/"
+    aggregate_folder = f"{sys.argv[1]}"
+    recipe_folder = f"{aggregate_folder}/{sys.argv[2]}-feedstock/recipe/"
     recipes = [recipe_folder]
-    linter = lint.Linter(config, recipe_folder, None)
+    linter = lint.Linter(config, aggregate_folder, None, True)
     result = linter.lint(recipes)
     messages = linter.get_messages()
-    pdb.set_trace()
+    #pdb.set_trace()
     print(linter.get_report())
 
     # if messages:
