fc5215fdef9cf82a17298e299f4469f22a2dd1de
Marco Esters <mesters@anaconda.com>
2022-09-23 08:52:05 -0700

    Add github.com -> githubusercontent.com redirect

--- anaconda_linter/lint/check_url.py
+++ anaconda_linter/lint/check_url.py
@@ -19,7 +19,10 @@ class invalid_url(LintCheck):
         url = source.get("url", "")
         if url:
             response_data = utils.check_url(url)
-            acceptable_redirects = [("pypi.io", "pypi.org")]
+            acceptable_redirects = [
+                ("pypi.io", "pypi.org"),
+                ("github.com", "objects.githubusercontent.com")
+            ]
             if response_data["code"] < 0 and "domain_redirect" in response_data:
                 for redir in acceptable_redirects:
                     if (
