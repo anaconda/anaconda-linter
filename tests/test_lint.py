4f67b8b9cc3ecbea1edc46651a3374eba84dda8f
Marco Esters <mesters@anaconda.com>
2022-10-11 17:36:39 -0700

    Add test_lint prototype

---
+++ tests/test_lint.py
@@ -0,0 +1,26 @@
+from anaconda_linter.recipe import Recipe
+import os
+import pytest
+try:
+    from ruamel.yaml import YAML
+except ModuleNotFoundError:
+    from ruamel_yaml import YAML
+
+
+yaml = YAML(typ="rt")  # pylint: disable=invalid-name
+with open(os.path.join(os.path.dirname(__file__), "test_lint.yaml"), "rt") as data:
+    TEST_DATA = yaml.load(data)
+TEST_IDS = [datum['name'] for datum in TEST_DATA]
+
+
+@pytest.mark.parametrize("test", TEST_DATA, ids=TEST_IDS)
+def test_lint(linter, base_yaml, test):
+    meta_yaml = yaml.load(base_yaml)
+    meta_yaml.update(test.get("add", {}))
+    recipe = Recipe.from_yaml(meta_yaml)
+    check = test.get('check')
+    messages = linter.check_instances[check].run(recipe=recipe)
+    if test.get('pass', True):
+        assert len(messages) == 0
+    else:
+        assert len(messages) == 1
