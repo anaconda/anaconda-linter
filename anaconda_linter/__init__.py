d2638d5dfb2aeb924b07e6634f8f975eaf8d17cb
Marco Esters <mesters@anaconda.com>
2022-10-13 07:39:07 -0700

    Change name/version of module

--- anaconda_linter/__init__.py
+++ anaconda_linter/__init__.py
@@ -1,5 +1,9 @@
 __name__ = "anaconda_linter"
+<<<<<<< HEAD
 __version__ = "0.0.4"
+=======
+__version__ = "0.0.3"
+>>>>>>> ade752f (Change name of module)
 __author__ = "Anaconda, Inc."
 __email__ = "distribution_team@anaconda.com"
 __license__ = "BSD-3-Clause"
