a1ca5142e77bf4a879e2a7cee6aa8a5728923259
Marco Esters <mesters@anaconda.com>
2022-10-26 07:06:39 -0700

    Lint file

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,12 +1,11 @@
-from anaconda_linter import lint
-from conftest import check
-
 import os
+
+from anaconda_linter import lint
 
 
 def test_lint_list():
     checks_file = os.path.abspath(os.path.dirname(__file__) + "/../anaconda_linter/lint_names.md")
     with open(checks_file) as f:
         lint_checks_file = [line.strip() for line in f.readlines() if line.strip()]
-    lint_checks_lint = [str(check) for check in lint.get_checks()]
+    lint_checks_lint = [str(chk) for chk in lint.get_checks()]
     assert sorted(lint_checks_file) == sorted(lint_checks_lint)
