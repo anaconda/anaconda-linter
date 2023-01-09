bf382b34c5ec348546c432a6cc482a0e157b815f
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2023-01-09 07:34:01 +0000

    get conda installed in gitpod on startup

--- get.sh
+++ get.sh
@@ -1,2 +1,5 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -u -p /workspace/anaconda-linter/miniconda3
+eval "$(/workspace/anaconda-linter/miniconda3/bin/conda shell.bash hook)"
+rm Miniconda3-latest-Linux-x86_64.sh
+conda init
