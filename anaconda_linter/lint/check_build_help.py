789125e03bcd37f0f3c94929c2496a05c197aed1
Marco Esters <mesters@anaconda.com>
2023-01-25 07:43:39 -0800

    Add fallbacks for get commands

--- anaconda_linter/lint/check_build_help.py
+++ anaconda_linter/lint/check_build_help.py
@@ -301,7 +301,9 @@ class uses_setup_py(LintCheck):
                 if script == "build/script":
                     build_file = "build.sh"
                 else:
-                    build_file = self.recipe.get(script)
+                    build_file = self.recipe.get(script, "")
+                if not build_file:
+                    continue
                 with open(os.path.join(self.recipe.dir, build_file)) as buildsh:
                     for num, line in enumerate(buildsh):
                         if not self._check_line(line):
@@ -346,7 +348,9 @@ class pip_install_args(LintCheck):
                     if script == "build/script":
                         build_file = "build.sh"
                     else:
-                        build_file = self.recipe.get(script)
+                        build_file = self.recipe.get(script, "")
+                    if not build_file:
+                        continue
                     with open(os.path.join(self.recipe.dir, build_file)) as buildsh:
                         for num, line in enumerate(buildsh):
                             if not self._check_line(line):
@@ -393,7 +397,7 @@ class cython_needs_compiler(LintCheck):
                 else:
                     section = "requirements/build"
                     output = -1
-                if "compiler_c" not in self.recipe.get(section):
+                if "compiler_c" not in self.recipe.get(section, ""):
                     self.message(section=section, output=output)
 
 
