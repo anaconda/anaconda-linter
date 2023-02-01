5c4c269381aaf1c56fa3bd48dff7a1fa8569b1f7
Marco Esters <mesters@anaconda.com>
2023-01-31 17:10:33 -0800

    Skip lists in the CBC file

--- anaconda_linter/recipe.py
+++ anaconda_linter/recipe.py
@@ -409,8 +409,11 @@ class Recipe:
         - normalize
         """
         try:
-            render_dict = {**self.JINJA_VARS, **self.selector_dict}
-            yaml_text = self.get_template().render(render_dict)
+            render_dict = {**self.JINJA_VARS}
+            for key, val in self.selector_dict.items():
+                if not isinstance(val, list):
+                    render_dict[key] = val
+            yaml_text = self.get_template().render(self.JINJA_VARS)
         except jinja2.exceptions.TemplateSyntaxError as exc:
             raise JinjaRenderFailure(self, message=exc.message, line=exc.lineno)
         except jinja2.exceptions.TemplateError as exc:
