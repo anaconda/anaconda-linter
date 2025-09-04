8119c3a702da942355a885ecf2740dbd10cd4263
Mohamed Sentissi <msentissi@anaconda.com>
2025-09-04 17:13:25 +0200

    update tests

--- tests/lint/test_multi_output.py
+++ tests/lint/test_multi_output.py
@@ -6,7 +6,7 @@ Description:    Tests multi-output-based rules
 from __future__ import annotations
 
 import pytest
-from conftest import check
+from conftest import assert_lint_messages, assert_no_lint_message, check
 
 
 def test_output_missing_name_good(base_yaml: str) -> None:
@@ -41,47 +41,27 @@ def test_output_missing_name_bad(base_yaml: str) -> None:
     assert len(messages) == 2 and all("has no name" in msg.title for msg in messages)
 
 
-def test_outputs_not_unique_good(base_yaml: str) -> None:
-    yaml_str = (
-        base_yaml
-        + """
-        outputs:
-          - name: output1
-          - name: output2
-        """
+@pytest.mark.parametrize(
+    "file,",
+    [
+        "outputs_not_unique/outputs_unique.yaml",
+    ],
+)
+def test_outputs_not_unique_outputs_unique(file: str) -> None:
+    assert_no_lint_message(recipe_file=file, lint_check="outputs_not_unique")
+
+
+@pytest.mark.parametrize(
+    "file,msg_count",
+    [
+        ("outputs_not_unique/outputs_not_unique.yaml", 2),
+        ("outputs_not_unique/outputs_not_unique_package_name.yaml", 1),
+    ],
+)
+def test_outputs_not_unique_outputs_not_unique(file: str, msg_count: str) -> None:
+    assert_lint_messages(
+        recipe_file=file, lint_check="outputs_not_unique", msg_title="Output name is not unique", msg_count=msg_count
     )
-    lint_check = "outputs_not_unique"
-    messages = check(lint_check, yaml_str)
-    assert len(messages) == 0
-
-
-def test_outputs_not_unique_bad_package_name(base_yaml: str) -> None:
-    yaml_str = (
-        base_yaml
-        + """
-        outputs:
-          - name: output1
-          - name: test_package
-        """
-    )
-    lint_check = "outputs_not_unique"
-    messages = check(lint_check, yaml_str)
-    assert len(messages) == 1 and "not unique" in messages[0].title
-
-
-def test_outputs_not_unique_bad(base_yaml: str) -> None:
-    yaml_str = (
-        base_yaml
-        + """
-        outputs:
-          - name: output1
-          - name: output2
-          - name: output2
-        """
-    )
-    lint_check = "outputs_not_unique"
-    messages = check(lint_check, yaml_str)
-    assert len(messages) == 1 and "not unique" in messages[0].title
 
 
 def test_no_global_test_good(base_yaml: str) -> None:
